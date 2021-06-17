185b67050d9565c4eaa3aa3a5c9ca15cc9c99ed6
Danil Vladimirov <danilvladimirovnkk@gmail.com>
2021-06-17 12:32:15 +0300

    fix

--- trembovetska/settings.py
+++ trembovetska/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-k3yu8yk)_7t-pe0^(0#_d*go(#5lv072%fb)d5ttga4-tq)-6h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trembovecka-site.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
